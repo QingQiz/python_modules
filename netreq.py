f012eb86466747fa067860437c92aba35ea038a8
QingQiz <sofeeys@outlook.com>
2020-07-24 16:27:06 +0800

    update netreq

--- netreq.py
+++ netreq.py
@@ -3,12 +3,14 @@
 
 
 import requests
+import parallel
 
 
-def login_aoxiang(username, password):
+def login_aoxiang(username, password, s=None):
     url = 'https://uis.nwpu.edu.cn/cas/login'
 
-    s = requests.Session()
+    if s is None:
+        s = requests.Session()
 
     s.headers.update({
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
@@ -30,3 +32,66 @@ def login_aoxiang(username, password):
     r = s.post(url, data=form_data)
 
     return s
+
+
+def url_content(url, encoding='', headers={}, data={}, params={}, s=None):
+    if s is None:
+        s = requests.Session()
+
+    r = s.post(url, data=data, params=params, headers=headers)
+
+    if r.encoding:
+        r.encoding = encoding
+
+    return r.content
+
+
+def url_html(url, encoding='', headers={}, data={}, params={}, s=None):
+    if s is None:
+        s = requests.Session()
+
+    r = s.post(url, data=data, params=params, headers=headers)
+
+    if r.encoding:
+        r.encoding = encoding
+
+    return r.text
+
+
+def url_json(url, encoding='', headers={}, data={}, params={}, s=None):
+    if s is None:
+        s = requests.Session()
+
+    r = s.post(url, data=data, params=params, headers=headers)
+
+    if r.encoding:
+        r.encoding = encoding
+
+    return r.json()
+
+
+def urls_content(urls, encoding='', headers={}, data={}, params={}, s=None, job=8):
+    if s is None:
+        s = requests.Session()
+
+    params = [[url, encoding, headers, data, params, s] for url in urls]
+
+    return parallel.init(job)(url_content, params)
+
+
+def urls_html(urls, encoding='', headers={}, data={}, params={}, s=None, job=8):
+    if s is None:
+        s = requests.Session()
+
+    params = [[url, encoding, headers, data, params, s] for url in urls]
+
+    return parallel.init(job)(url_html, params)
+
+
+def urls_json(urls, encoding='', headers={}, data={}, params={}, s=None, job=8):
+    if s is None:
+        s = requests.Session()
+
+    params = [[url, encoding, headers, data, params, s] for url in urls]
+
+    return parallel.init(job)(url_json, params)
