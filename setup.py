354ab771fd912f863e4cc24eaacf0f5f8c264392
QingQiz <sofeeys@outlook.com>
2020-09-09 00:37:58 +0800

    doc: update readme

--- setup.py
+++ setup.py
@@ -4,16 +4,12 @@
 import setuptools
 
 
-with open("README.md") as f:
-    ld = f.read()
-
 setuptools.setup(
     name="QingQiz",
     version="0.0.1",
     author="QingQiz",
     author_email="sofeeys@outlook.com",
     description="..",
-    long_description=ld,
     long_description_content_type="text/markdown",
     url="https://github.com/QingQiz/python_scripts",
     packages=setuptools.find_packages(),
